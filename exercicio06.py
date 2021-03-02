16642d99035f3ef9ec0375d5064564723f2827d6
shidaan <danielframosp@gmail.com>
2021-03-02 20:33:43 -0300

    Exercício 6 do curso em vídeo fase 1
    Calcular o dobro, triplo e raiz quadrada de um número digitado

---
+++ exercicio06.py
@@ -0,0 +1,5 @@
+# desafio 06 -- dobro, triplo e raiz quadrada de um número
+import math
+
+n = int(input('Digite um número: '))
+print(f'O dobro de {n} é {n*2}.\nO triplo de {n} é {n*3}.\nA raiz quadrada de {n} é {math.sqrt(n)}.')
