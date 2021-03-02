e78d6e9068b68377c4f1f19eb5f17ff589a82e43
shidaan <79770539+shidaan@users.noreply.github.com>
2021-03-02 15:18:10 -0300

    Revert "mudança no curso_em_video1"
    This reverts commit 97ea009289ce211267d401fc3a6ad2ef1fd1557a.

--- curso_em_video1.py
+++ curso_em_video1.py
@@ -31,10 +31,10 @@
 # print('Oká ' + nome + '! Prazer em te conhecer!')
 
 # # desafio 2 ------------------------------
-dia = input('Dia = ')
-mes = input('Mês = ')
-ano = input('Ano = ')
-print('Você nasceu no dia ' + dia + ' de ' + mes + ' de ' + ano + '. Correto?')
+# dia = input('Dia = ')
+# mes = input('Mês = ')
+# ano = input('Ano = ')
+# print('Você nasceu no dia ' + dia + ' de ' + mes + ' de ' + ano + '. Correto?')
 
 # desafio 3 ------------------------------
 n1 = int(input('Primeiro número: '))
@@ -54,5 +54,3 @@ if info.isalnum():
         print(f'{info} está todo em minusculo')
 else:
     print(f'{info} não é alpha nem numérico')
-
-
