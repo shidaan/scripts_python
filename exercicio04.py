f43c77492352e68e6c3e17a6e67b23fb66f6adf0
shidaan <danielframosp@gmail.com>
2021-03-02 20:32:50 -0300

    Exercício 4 do curso em vídeo fase 1
    Dissecando uma variável

---
+++ exercicio04.py
@@ -0,0 +1,10 @@
+# desafio 04 - dissecando uma variável
+var = input('Digite algo: ')
+print(f'O tipo primitivo da variável é: {type(var)}')
+print(f'Só tem espaços? {var.isspace()}')
+print(f'É um número? {var.isnumeric()}')
+print(f'É alfabético? {var.isalpha()}')
+print(f'É alfanumérico? {var.isalnum()}')
+print(f'Está em maiúsculas? {var.isupper()}')
+print(f'Está em minúsculas? {var.islower()}')
+print(f'Está capitalizada? {var.istitle()}')
