8a57a698f76fdb0d075051c57d2e6f5106ff0f78
shidaan <danielframosp@gmail.com>
2021-03-02 20:37:55 -0300

    Exercício 13 do curso em vídeo fase 1
    Ler um salário e dar 15% de aumento

---
+++ exercicio13.py
@@ -0,0 +1,3 @@
+# desafio 13 -- ler um salário e dar 15% de aumento
+salario = float(input('Digite o salário: R$ '))
+print(f'Seu salário de R$ {salario} subiu para R$ {salario + (salario * 0.15):.2f}!')
