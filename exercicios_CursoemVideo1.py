0a454f59950dba9a0a19c6b12cc0d16b1cd3f6dd
shidaan <danielframosp@gmail.com>
2021-03-02 16:03:25 -0300

    Update exercicios_CursoemVideo1.py
    Comentários nos desafios de 1 à 3.
    Mudança no desafio 4.

--- exercicios_CursoemVideo1.py
+++ exercicios_CursoemVideo1.py
@@ -1,25 +1,23 @@
-# desafio 001
-print('Olá, Mundo!')
+# # desafio 001
+# print('Olá, Mundo!')
 
-# desafio 002
-nome = input('Digite seu nome: ')
-print(f'Prazer em te conhecer, {nome}!')
+# # desafio 002
+# nome = input('Digite seu nome: ')
+# print(f'Prazer em te conhecer, {nome}!')
 
-# desafio 003
-n1 = int(input('Primeiro número: '))
-n2 = int(input('Segundo número: '))
-print(f'A soma entre {n1} e {n2} é = {n1 + n2}')
-# print('A soma é ', (n1 + n2))
+# # desafio 003
+# n1 = int(input('Primeiro número: '))
+# n2 = int(input('Segundo número: '))
+# print(f'A soma entre {n1} e {n2} é = {n1 + n2}')
+# # print('A soma é ', (n1 + n2))
 
 # desafio 4
 info = input('Digite algo: ')
-print(type(info))
-
-if info.isalnum():
-    print(f'{info} é alpha ou numérico')
-    if info.isupper():
-        print(f'{info} está todo em maiusculo')
-    else:
-        print(f'{info} está todo em minusculo')
-else:
-    print(f'{info} não é alpha nem numérico')
+print(f'O tipo primitivo desse valor é: {type(info)}')
+print(f'Só tem espaços? {info.isspace()}')
+print(f'É um número? {info.isnumeric()}')
+print(f'É alfabético? {info.isalpha()}')
+print(f'É alfanumérico? {info.isalnum()}')
+print(f'Está em maiúsculas? {info.isupper()}')
+print(f'Está em minúsculas? {info.islower()}')
+print(f'Está capitalizada? {info.istitle()}')
