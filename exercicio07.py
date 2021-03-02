ba3f4d83ee2225f622378730185a4b7596df63cd
shidaan <danielframosp@gmail.com>
2021-03-02 20:34:12 -0300

    Exercício 7 do curso em vídeo fase 1
    Ler duas notas e calcular sua média

---
+++ exercicio07.py
@@ -0,0 +1,4 @@
+# desafio 07 -- média de 2 notas
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+print(f'A média entre as notas {n1} e {n2} é {(n1+n2)/2:.1f}.')
