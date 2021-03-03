6364165d9fcdfcd5dfa8bdceeda27ef7af613075
shidaan <danielframosp@gmail.com>
2021-03-03 19:32:06 -0300

    Exercicio14 do curso em video fase 1
    conversor de temperatura, celsius para fahrenheit

---
+++ exercicio14.py
@@ -0,0 +1,6 @@
+# desafio 14 -- converter temperatura em celsius para fahrenheit
+celsius = float(input('Qual a temepratura em Celsius(ºC): '))
+
+fahrenheit = ((9 * celsius) / 5) + 32
+
+print(f'A temperatura de {celsius:.1f}ºC ficou {fahrenheit:.1f}ºF (Fahrenheit)')
