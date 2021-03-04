f6d83c049626b1b13136d8ce6ff5e979a2f8d399
shidaan <danielframosp@gmail.com>
2021-03-04 19:23:34 -0300

    Atualização Exercicio21
    adicionado pasta mp3 e diretório no código

--- exercicio21.py
+++ exercicio21.py
@@ -3,7 +3,7 @@
 import pygame
 
 pygame.init()
-pygame.mixer.music.load('exercicio21.mp3')
+pygame.mixer.music.load('mp3/exercicio21.mp3')
 pygame.mixer.music.play()
-pygame.mixer.wait()
-# x = input('Digite algo para parar a música')
+# pygame.event.wait()
+x = input('Digite algo para parar a música')
