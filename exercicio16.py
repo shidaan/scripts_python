6b12b66cf821dff8df63dda60f53b895442cb577
shidaan <danielframosp@gmail.com>
2021-03-04 19:04:04 -0300

    Exercício16 curso em vídeo
    ler número real e mostrar a parte inteira

---
+++ exercicio16.py
@@ -0,0 +1,7 @@
+import math
+# ler um número REAL e mostrar na tela sua porção inteira
+# trunc - retorna a parte inteira do número float
+
+num = float(input('Digite um número REAL: '))
+
+print(f'A parte inteira de {num} é {math.trunc(num)}')
