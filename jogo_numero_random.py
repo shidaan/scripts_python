145132d0f02dfd2aa695efe882c5c6152c4197bd
shidaan <danielframosp@gmail.com>
2021-03-04 20:44:12 -0300

    Atualização jogo número random
    adição  de novo while para loop do programa mesmo com erro de exceção, e mudança na forma de importar a biblioteca

--- jogo_numero_random.py
+++ jogo_numero_random.py
@@ -1,21 +1,26 @@
-import random as r
+from random import randint
 
-num_comp = r.randint(1,100)
+num_comp = randint(1, 100)
+break_2 = True
+count_erros = 0
 
-try:
-    while True:    
+while break_2 == True:
+    try:
+        while True:
 
-        n = int(input('Adivinhe um número de 1 ao 100: '))
-        print(num_comp)
+            n = int(input('Adivinhe um número de 1 ao 100: '))
 
+            if n < num_comp:
+                print('O número é \033[1;36m\u2191\033[m')  # Maior
+                count_erros += 1
+            elif n > num_comp:
+                print('O número é \033[1;36m\u2193\033[m')  # Menor
+                count_erros += 1
+            elif n == num_comp:
+                print(
+                    f'\n\033[1;32mParabéns!\033[m \nVocê acertou na \033[1;31m{count_erros}ª\033[m tentativa!')
+                break_2 = False
+                break
 
-        if n < num_comp:
-            print('O número é \u2191')
-        elif n > num_comp:
-            print('O número é \u2193')
-        elif n == num_comp:
-            print('Parabéns')
-            break 
-        
-except ValueError:
+    except ValueError:
         print('Somente números inteiros')
