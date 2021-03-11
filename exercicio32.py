eb4cf253d683e5370c35a59b90989e333c1d195d
shidaan <danielframosp@gmail.com>
2021-03-11 14:40:55 -0300

    Atualização do Exercicio32 - curso em vídeo
    Caso usuário digite 0 irá analisar o ano atual

--- exercicio32.py
+++ exercicio32.py
@@ -1,6 +1,11 @@
 # exercicio 32 - ano bisexto 
 
-ano = int(input('Digite o ano: '))
+from datetime import date
+
+ano = int(input('Digite o ano ou digite 0 para analisar o ano atual: '))
+
+if ano == 0:
+    ano = date.today().year  # da biblioteca date, chama o método today() apontando o valor para ano 'date.today().year'
 
 if ano % 100 == 0 and (ano / 100) % 4 == 0:
     print(f'O ano \033[1;32m{ano}\033[m é Bissexto!')
