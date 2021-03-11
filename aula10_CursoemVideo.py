76620cbfb3a41b13acf18fff18535f237f4269ad
shidaan <danielframosp@gmail.com>
2021-03-11 12:58:12 -0300

    aula10_CursoemVideo
    Estudo de condições if e else

---
+++ aula10_CursoemVideo.py
@@ -0,0 +1,5 @@
+# else e if de maneira simplificada
+
+numero = int(input('Digite um número: '))
+
+print(f'O número {numero} é maior ou igual à 10' if numero >= 10 else f'O número {numero} é menor que 10')
